59310477833c902a377edf53fd69fb0e50914497
AnjaP98 <97261181+AnjaP98@users.noreply.github.com>
2022-01-28 02:31:22 +0100

    ispravljene greske

--- knjige.php
+++ knjige.php
@@ -25,36 +25,8 @@ class Knjiga
         return $connection->query($q);
     }
 
-    /*public static function getByLanguage($jezik, mysqli $connection)
-    {
-        $q = "SELECT * FROM knjige WHERE jezik=$jezik";
-        $myArray = array();
-        if ($result = $connection->query($q)) {
-
-            while ($row = $result->fetch_array(1)) {
-                $myArray[] = $row;
-            }
-        }
-        return $myArray;
-    }
-    public static function getByCategory($zanr, mysqli $connection)
-    {
-        $q = "SELECT * FROM knjige WHERE zanr=$zanr";
-        $myArray = array();
-        if ($result = $connection->query($q)) {
-
-            while ($row = $result->fetch_array(1)) {
-                $myArray[] = $row;
-            }
-        }
-        return $myArray;
-    }*/
-    public function deleteById(mysqli $conn)
-    {
-        $query = "DELETE FROM knjige WHERE sifra=$this->id";
-        return $conn->query($query);
-    }
-
+    
+    
     
 
     public static function dodajKnjigu($id, $naziv,$zanr, $autor, $cena, $jezik, mysqli $connection)
